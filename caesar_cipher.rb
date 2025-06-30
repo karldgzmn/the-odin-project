001fc6d41c62c037541295d2e25687fc6669f09b
Karl De Guzman <165979767+karldgzmn@users.noreply.github.com>
2025-06-30 23:11:38 +0000

    add

---
+++ caesar_cipher.rb
@@ -0,0 +1,19 @@
+def caesar_cipher(text, shift)
+    upper = ('A'..'Z').to_a
+    lower = ('a'..'z').to_a
+
+    result = text.chars.map do |char|
+      if upper.include?(char)
+        upper[(upper.index(char) + shift) % 26]
+      else if lower.include?(char)
+        lower[(lower.index(char) + shift) % 26]
+      else
+      char
+    end
+  end
+end
+
+  result.join
+end
+
+puts caesar_cipher("What a string!", 5)
